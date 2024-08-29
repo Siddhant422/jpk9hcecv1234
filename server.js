850fac80084c2da58fc9f52ee9cd80ab00c81dab
Siddhant Keshari <siddhantkeshari76@gmail.com>
2024-08-30 00:18:14 +0530

    changes done

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config({ path: './config.env' });
 const app = require('./app');
 const mongoose = require('mongoose');
 const PORT = 4000;
 
-const db = "mongodb+srv://siddhantkeshari:j3pk9hcecv@cluster0.60hdo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const db = process.env.DB
 
 mongoose.connect(db).then(() => {
     console.log('Connected to database');
